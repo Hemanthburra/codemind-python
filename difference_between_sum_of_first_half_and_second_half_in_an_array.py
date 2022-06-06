e5ed45e8f3b0d0374ce30675f3a30d0725ce8487
Hemanthburra <102217798+Hemanthburra@users.noreply.github.com>
2022-06-06 19:20:38 +0530

    x=int(input()) s=list(map(int,input().split())) d=len(s)//2 sum1=0 sum2=0 for i in range(len(s)):     if i>=d:         sum1+=s[i]     else:         sum2+=s[i] print(sum1-sum2) #print(sum1)

---
+++ difference_between_sum_of_first_half_and_second_half_in_an_array.py
@@ -0,0 +1,12 @@
+x=int(input())
+s=list(map(int,input().split()))
+d=len(s)//2
+sum1=0
+sum2=0
+for i in range(len(s)):
+    if i>=d:
+        sum1+=s[i]
+    else:
+        sum2+=s[i]
+print(sum1-sum2)
+#print(sum1)
