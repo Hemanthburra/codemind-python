d2ee85d1eff947089a0ceca0a87612fb4f8536be
Hemanthburra <102217798+Hemanthburra@users.noreply.github.com>
2022-06-07 17:59:23 +0530

    a,b=map(int,input().split()) f=list(map(int,input().split())) g=list(map(int,input().split())) d=[]
    for i in range(len(f)):
        if f[i] in g:
            if f[i] not in d:
                d.append(f[i])
    print(len(d))

---
+++ Count_elements_I.py
@@ -0,0 +1,10 @@
+a,b=map(int,input().split())
+f=list(map(int,input().split()))
+g=list(map(int,input().split()))
+d=[]
+
+for i in range(len(f)):
+    if f[i] in g:
+        if f[i] not in d:
+            d.append(f[i])
+print(len(d))
