66f170bbb7774e7fc65215da052289b5fe6fdc64
Hemanthburra <102217798+Hemanthburra@users.noreply.github.com>
2022-09-12 12:20:07 +0530

    s=input() t=['a','e','i','o','u','A','E','I','O','U'] s=s.split() #print(s) y=[] for i in range(len(s)):     p=s[i]     #print(p)     c=0     for j in range(len(p)):         #print(p[i])         if p[j] in t:             c+=1     y.append(c) print(*y)

---
+++ vowel_count_in_each_word.py
@@ -0,0 +1,15 @@
+s=input()
+t=['a','e','i','o','u','A','E','I','O','U']
+s=s.split()
+#print(s)
+y=[]
+for i in range(len(s)):
+    p=s[i]
+    #print(p)
+    c=0
+    for j in range(len(p)):
+        #print(p[i])
+        if p[j] in t:
+            c+=1
+    y.append(c)
+print(*y)
