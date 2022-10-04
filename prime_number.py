e4c13f032c5aebb673c1e8374400e434cfb9466c
Hemanthburra <102217798+Hemanthburra@users.noreply.github.com>
2022-10-04 11:39:06 +0530

    def isprime(n):     import math     if n<=1:         return False     for i in range(2,int(math.sqrt(n))+1):         if n%i==0:             return False     return True
    a=int(input())
    if isprime(a):
        print("prime")
    else:
        print("not a prime")

---
+++ prime_number.py
@@ -0,0 +1,14 @@
+def isprime(n):
+    import math
+    if n<=1:
+        return False
+    for i in range(2,int(math.sqrt(n))+1):
+        if n%i==0:
+            return False
+    return True
+
+a=int(input())
+if isprime(a):
+    print("prime")
+else:
+    print("not a prime")
