90bfc704afbbeab8ea83ebfbcc2656b49602fa9c
Hemanthburra <102217798+Hemanthburra@users.noreply.github.com>
2022-10-07 00:03:43 +0530

    import math def isprime(n):     if n<=1:         return False     for i in range(2,int(math.sqrt(n))+1):         if n%i==0:             return False     return True t=int(input()) t1=int(input()) cnt=0 for i in range(t,t1+1):     if isprime(i):         cnt+=1 print(cnt)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,16 +1,18 @@
 import math
-def sq(n):
+def isprime(n):
     if n<=1:
         return False
-    for i in range(2,int(math.sqrt(n)+1)):
+    for i in range(2,int(math.sqrt(n))+1):
         if n%i==0:
             return False
     return True
-
-a=int(input())
-b=int(input())
+t=int(input())
+t1=int(input())
 cnt=0
-for i in range(a,b+1):
-    if sq(i):
+for i in range(t,t1+1):
+    if isprime(i):
         cnt+=1
 print(cnt)
+    
+
+        
