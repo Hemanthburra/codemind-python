6286276980a3d0930993ad0f4704d59c799bec41
Hemanthburra <102217798+Hemanthburra@users.noreply.github.com>
2022-10-06 22:58:14 +0530

    n=int(input()) cmt=0 arr=n*[None] for i in range(n):     arr[i]=int(input()) t=int(input()) #print(n,arr,t) for j in range(len(arr)):     if arr[j]<=t:         cmt+=1     else:         cmt+=2 print(cmt)

---
+++ Airport__authority.py
@@ -0,0 +1,13 @@
+n=int(input())
+cmt=0
+arr=n*[None]
+for i in range(n):
+    arr[i]=int(input())
+t=int(input())
+#print(n,arr,t)
+for j in range(len(arr)):
+    if arr[j]<=t:
+        cmt+=1
+    else:
+        cmt+=2
+print(cmt)
