0f2ae7a306b457e5d17379667a7de847554c54c0
Hemanthburra <102217798+Hemanthburra@users.noreply.github.com>
2022-10-06 20:15:00 +0530

    t=int(input()) for _ in range(t):     r=int(input())     r=list(str(r))     r.reverse()     s=0     for i in range(len(r)):         s+=int(r[i])*(2**i)     print(s)

---
+++ Binary_to_Decimal.py
@@ -0,0 +1,10 @@
+t=int(input())
+for _ in range(t):
+    r=int(input())
+    r=list(str(r))
+    r.reverse()
+    s=0
+    for i in range(len(r)):
+        s+=int(r[i])*(2**i)
+    print(s)
+        
