4402921663afa51ecfa911599b0c927c92ca1388
Hemanthburra <102217798+Hemanthburra@users.noreply.github.com>
2022-10-06 21:33:20 +0530

    import math def isprime(n):     if n<=1:         return False     for i in range(2,int(math.sqrt(n))+1):         if n%i==0:             return False     return True
    t=int(input())
    for _ in range(t):
        a=int(input())
        l=[]
        for i in range(1,a+a):
            if isprime(i):
                l.append(i)
                if i>a:
                    break
        if abs(l[-1]-a)<abs(l[-2]-a):
            print(l[-1])
        elif abs(l[-1]-a)>=abs(l[-2]-a):
            print(l[-2])

---
+++ Nearest_Prime.py
@@ -0,0 +1,25 @@
+import math
+def isprime(n):
+    if n<=1:
+        return False
+    for i in range(2,int(math.sqrt(n))+1):
+        if n%i==0:
+            return False
+    return True
+
+t=int(input())
+for _ in range(t):
+    a=int(input())
+    l=[]
+    for i in range(1,a+a):
+        if isprime(i):
+            l.append(i)
+            if i>a:
+                break
+    if abs(l[-1]-a)<abs(l[-2]-a):
+        print(l[-1])
+    elif abs(l[-1]-a)>=abs(l[-2]-a):
+        print(l[-2])
+    
+        
+    
